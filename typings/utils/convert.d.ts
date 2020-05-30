5be6936c9bc4218a8f626a55850f8a42e2369cab
William Johnston <johnston.w@gmail.com>
2020-05-30 09:10:06 -0700

    removing typings

--- typings/utils/convert.d.ts
+++
@@ -1,7 +0,0 @@
-export declare function identity<T = any>(value?: T): T;
-export declare function forEach<T>(iterator: (value: T, index: number, obj: any) => void, array: T[], context?: any): T[];
-export declare function forEach<T>(iterator: (value: T, key: string, obj: any) => void, obj: any, context?: any): any;
-export declare function noop(...args: any[]): void;
-export declare function extend(deep: boolean, destination: any, ...sources: any[]): any;
-export declare function clone<T>(obj: T): T;
-export declare function shallowClone<T>(obj: T): T;
