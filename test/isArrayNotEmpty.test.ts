36a1609e6ed5c0216e98c2799bbf6058ef4bd2f0
William Johnston <johnston.w@gmail.com>
2020-07-11 15:37:26 -0700

    test stubs

--- test/isArrayNotEmpty.test.ts
+++ test/isArrayNotEmpty.test.ts
@@ -1,8 +1,17 @@
-import identity from '../src/identity';
+import isArrayNotEmpty from '../src/isArrayNotEmpty';
 
-test('identity should return what you pass to it', () => {
-    expect(identity()).toBeUndefined();
-    expect(identity(undefined)).toBeUndefined();
-    expect(identity(null)).toBeNull();
-    expect(identity(2)).toBe(2);
+test('isArrayNotEmpty should return true for arraylike objects with length > 0', () => {
+    expect(isArrayNotEmpty([1])).toBe(true);
+    expect(isArrayNotEmpty(new Array(3))).toBe(true);
+    expect(isArrayNotEmpty(/123/.exec('123'))).toBe(true);
+    expect(isArrayNotEmpty('abc')).toBe(true);
+    expect(isArrayNotEmpty({ length: 2 })).toBe(true);
+});
+
+test('isArrayNotEmpty should return false for non-arraylike and arraylike object with length <= 0', () => {
+    expect(isArrayNotEmpty(undefined)).toBe(false);
+    expect(isArrayNotEmpty(null)).toBe(false);
+    expect(isArrayNotEmpty(new Array())).toBe(false);
+    expect(isArrayNotEmpty([])).toBe(false);
+    expect(isArrayNotEmpty({ length: 0 })).toBe(false);
 });
